3df6e9dc12d4270d3e67c8e499150d26bca1058c
stheffani27 <sbro@aluno.ifnmg.edu.br>
2026-04-08 20:51:09 +0000

    docs : codigo inserido

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,25 +7,55 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  // escreva seu código aqui
+   if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else {
+    return "adulto";
+  }
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+  return preco - (preco * percentual / 100);
+
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+   if (nota >= 60) {
+    return "aprovado";
+  } else if (nota >= 40) {
+    return "recuperação";
+  } else {
+    return "reprovado";
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+   let soma = 0;
+
+  for (const numero of numeros) {
+    if (numero % 2 === 0) {
+      soma += numero;
+    }
+  }
+
+  return soma;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+   let contador = 0;
+
+  for (const caractere of texto) {
+    if (caractere === letra) {
+      contador++;
+    }
+  }
+
+  return contador;
+
 }
