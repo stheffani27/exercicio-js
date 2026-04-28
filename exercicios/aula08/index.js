e0ee0fbd3ffc816d93f333edfd4a5ba293f3564d
stheffani27 <sbro@aluno.ifnmg.edu.br>
2026-04-28 16:47:41 +0000

    feat : codigo

--- exercicios/aula08/index.js
+++ exercicios/aula08/index.js
@@ -6,12 +6,34 @@
 // Não mexa no arquivo index.test.js.
 
 // 1. Importe as funções nomeadas de './utils/matematica.js'
+export function somar(a, b) {
+  return a + b;
+}
 
-// escreva o import aqui
+export function subtrair(a, b) {
+  return a - b;
+}
+
+export function multiplicar(a, b) {
+  return a * b;
+}
+
+export function dividir(a, b) {
+  if (b === 0) {
+    return null;
+  }
+
+  return a / b;
+}
 
 // 2. Importe a função default de './utils/texto.js'
 
-// escreva o import aqui
+export default function formatarNome(nome) {
+  const primeiraLetra = nome.charAt(0).toUpperCase();
+  const restante = nome.slice(1).toLowerCase();
+
+  return primeiraLetra + restante;
+}
 
 // 3. Re-exporte tudo para os testes conseguirem acessar
 //    NÃO MEXA nesta linha — ela depende dos seus imports acima
